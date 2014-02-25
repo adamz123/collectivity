6299537491162c4b5df8bd2b56950344ec89b40c
Adam Zeldis <amzeldis@gmail.com>
2014-02-25 01:51:36 -0500

    add lib and tests

---
+++ tests/test.js
@@ -0,0 +1,29 @@
+var Collectivity = require('../lib/collectivity.js');
+
+var keywords = {
+	facebook: 	[{keyword: 'recession', lastChecked: '1388099510000'}],
+	news: 		[{keyword: 'recession', lastChecked: '1388099510000'}],
+	twitter: 	[{keyword: 'recession', max_id: '0'}]
+};
+
+
+var linksReturned = function(err, links) {
+	if(err){
+		console.log('An error occurred:');
+		console.log(err);
+	} else {
+		console.log('The following links were returned:');
+		console.log(links);
+	}
+}
+
+
+var keywordsReturned = function(type, keywords) {
+	console.log('The following ' + type + ' keyword dictionary was returned:');
+	console.log(keywords);
+}
+
+
+var recessionCollectivity = new Collectivity(keywords);
+
+recessionCollectivity.aggregate(linksReturned, keywordsReturned);
