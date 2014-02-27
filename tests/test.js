1f33e2dd4dd845c1d643d0f7f3f903d50b696fb7
Adam Zeldis <amzeldis@gmail.com>
2014-02-26 23:40:01 -0500

    jshinted

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,9 @@
 var Collectivity = require('../lib/collectivity.js');
 
 var keywords = {
-	facebook: 	[{keyword: 'recession', lastChecked: '1388099510000'}],
-	news: 		[{keyword: 'recession', lastChecked: '1388099510000'}],
-	twitter: 	[{keyword: 'recession', max_id: '0'}]
+	facebook:	[{keyword: 'recession', lastChecked: '1388099510000'}],
+	news:		[{keyword: 'recession', lastChecked: '1388099510000'}],
+	twitter:	[{keyword: 'recession', max_id: '0'}]
 };
 
 
@@ -15,13 +15,13 @@ var linksReturned = function(err, links) {
 		console.log('The following links were returned:');
 		console.log(links);
 	}
-}
+};
 
 
 var keywordsReturned = function(type, keywords) {
 	console.log('The following ' + type + ' keyword dictionary was returned:');
 	console.log(keywords);
-}
+};
 
 
 var recessionCollectivity = new Collectivity(keywords);
