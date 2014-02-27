be803d18b9967c18aa219008ae39c832f9d7ac4d
Adam Zeldis <amzeldis@gmail.com>
2014-02-26 23:45:45 -0500

    fix whitspacing

--- tests/test.js
+++ tests/test.js
@@ -1,26 +1,26 @@
 var Collectivity = require('../lib/collectivity.js');
 
 var keywords = {
-	facebook:	[{keyword: 'recession', lastChecked: '1388099510000'}],
-	news:		[{keyword: 'recession', lastChecked: '1388099510000'}],
-	twitter:	[{keyword: 'recession', max_id: '0'}]
+    facebook:   [{keyword: 'recession', lastChecked: '1388099510000'}],
+    news:       [{keyword: 'recession', lastChecked: '1388099510000'}],
+    twitter:    [{keyword: 'recession', max_id: '0'}]
 };
 
 
 var linksReturned = function(err, links) {
-	if(err){
-		console.log('An error occurred:');
-		console.log(err);
-	} else {
-		console.log('The following links were returned:');
-		console.log(links);
-	}
+    if(err){
+        console.log('An error occurred:');
+        console.log(err);
+    } else {
+        console.log('The following links were returned:');
+        console.log(links);
+    }
 };
 
 
 var keywordsReturned = function(type, keywords) {
-	console.log('The following ' + type + ' keyword dictionary was returned:');
-	console.log(keywords);
+    console.log('The following ' + type + ' keyword dictionary was returned:');
+    console.log(keywords);
 };
 
 
